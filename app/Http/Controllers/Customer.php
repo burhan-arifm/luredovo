153733bea242f03c2c00d42c59d1b28812b5ebc5
Burhan Arif M <burhan.arifm@hotmail.com>
2019-12-10 16:42:31 +0700

    85%, JSON response, and request validation

--- app/Http/Controllers/Customer.php
+++ app/Http/Controllers/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Redis;
@@ -10,19 +11,28 @@ class Customer extends Controller
 {
     public function register(Request $request)
     {
+        $this->validate($request, [
+            'name'  => 'required',
+            'phone' => 'required',
+            'email' => 'required|email'
+        ]);
+        
+        $customer               = new \stdClass();
+        $customer->id           = "customer:".$request->input('phone');
         $customer->name         = $request->input('name');
         $customer->phone        = $request->input('phone');
         $customer->email        = $request->input('email');
         $customer->balance      = $request->input('balance', 0);
-        $customer->premium      = false;
-        $customer->created_at   = Carbon::now();
+        $customer->premium      = 'false';
+        $customer->created_at   = Carbon::now()->format('Y-m-d H:m:s');
 
-        Redis::hMSet("customer:$phone", [
+        Redis::hMSet($customer->id, [
             'name'                  => $customer->name,
+            'phone'                 => $customer->phone,
             'email'                 => $customer->email,
             'balance'               => $customer->balance,
             'premium'               => $customer->premium,
-            'transactions_history'  => "history:$phone",
+            'transactions_history'  => "history:$customer->phone",
             'created_at'            => $customer->created_at,
             'updated_at'            => $customer->created_at
         ]);
@@ -37,7 +47,7 @@ class Customer extends Controller
 
         for ($index=0; $index < count($customer_keys); $index++)
         {
-            array_push($customers, customer($customer_keys[$index]));
+            array_push($customers, $this->customer($customer_keys[$index]));
         }
 
         return response()->json([ 'customers' => $customers ], 200);
@@ -45,24 +55,30 @@ class Customer extends Controller
 
     public function detail($customer_id)
     {
-        return response()->json([ 'customer' => customer($customer_id) ], 200);
+        if (Redis::keys("customer:$customer_id")) {
+            return response()->json([ 'customer' => $this->customer("customer:$customer_id") ], 200);
+        } else {
+            return response()->json([ 'err_message' => "Customer not found." ], 404);
+        }
+        
     }
 
     public function update(Request $request, $customer_id)
     {
-        $customer = customer($customer_id);
+        $customer = $this->customer($customer_id);
     }
 
     public function customer($id)
     {
-        $customer_key = "user:".$id;
+        $customer = new \stdClass();
 
-        $customer->name         = Redis::hGet($customer_key, 'name');
-        $customer->phone        = Redis::hGet($customer_key, 'phone');
-        $customer->email        = Redis::hGet($customer_key, 'email');
-        $customer->balance      = Redis::hGet($customer_key, 'balance');
-        $customer->premium      = Redis::hGet($customer_key, 'premium');
-        $customer->created_at   = Redis::hGet($customer_key, 'created_at');
+        $customer->id           = $id;
+        $customer->name         = Redis::hGet($customer->id, 'name');
+        $customer->phone        = Redis::hGet($customer->id, 'phone');
+        $customer->email        = Redis::hGet($customer->id, 'email');
+        $customer->balance      = Redis::hGet($customer->id, 'balance');
+        $customer->premium      = Redis::hGet($customer->id, 'premium');
+        $customer->created_at   = Redis::hGet($customer->id, 'created_at');
 
         return $customer;
     }
--- app/Http/Controllers/Transaction.php
+++ app/Http/Controllers/Transaction.php
@@ -9,38 +9,42 @@ use Illuminate\Support\Facades\Redis;
 
 class Transaction extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function make(Request $request, $customer_id)
     {
-        $timestamp = Carbon::now()->timestamp;
-
-        Redis::hMSet("transaction:$timestamp", [
-            'sender'        => $customer_id,
-            'receiver'      => $request->receiver,
-            'amount'        => $request->amount,
-            'type'          => $request->type,
-            'description'   => $request->description,
-            'created_at'    => Carbon::now()->timestamp,
+        $this->validate($request, [
+            'receiver_type' => 'required',
+            'receiver'      => 'required',
+            'amount'        => 'required',
+            'description'   => 'required'
         ]);
         
-        Redis::hSet("user:$customer_id", 'balance', Redis::hGet("user:$customer_id", 'balance') - $request->amount);
+        $transaction                = new \stdClass();
+        $transaction->id            = "transaction:".Carbon::now()->timestamp;
+        $transaction->sender        = "customer:$customer_id";
+        $transaction->receiver      = $request->input('receiver_type').':'.$request->input('receiver');
+        $transaction->amount        = $request->input('amount');
+        $transaction->promo         = $request->input('promo', 'false');
+        $transaction->description   = $request->input('description');
+        $transaction->created_at    = Carbon::now()->format('Y-m-d H:m:s');
+
+        Redis::hMSet($transaction->id, [
+            'sender'        => $transaction->sender,
+            'receiver'      => $transaction->receiver,
+            'amount'        => $transaction->amount,
+            'promo'         => $transaction->promo,
+            'description'   => $transaction->description,
+            'created_at'    => $transaction->created_at,
+        ]);
+        
+        Redis::hSet("customer:$customer_id", 'balance', Redis::hGet("customer:$customer_id", 'balance') - $request->amount);
         Redis::rPush("history:$customer_id", "transaction:$timestamp");
         
-        if (strpos($request->receiver, 'user:') || strpos($request->receiver, 'merchant:')) {
+        if (strpos($request->receiver, 'customer:')) {
             Redis::hSet($request->receiver, 'balance', Redis::hGet($request->receiver, 'balance') + $request->amount);
             Redis::rPush($request->receiver, "transaction:$timestamp");
         }
 
-        return response()->json([], 201);
+        return response()->json([ 'transaction' => $transaction ], 201);
     }
 
     public function all($customer_id)
@@ -48,22 +52,26 @@ class Transaction extends Controller
         $histories = [];
 
         while (count($histories) < Redis::lLen("history:$customer_id")) {
-            $history_id = Redis::lPop("history:$customer_id");
-            $sender = Redis::hGet($history_id, 'sender');
-            if ($sender != "user:$customer_id") {
-                $history->sender->phone = Redis::hGet($sender, 'phone');
+            $transaction->id = Redis::lPop("history:$customer_id");
+            $sender = Redis::hGet($transaction->id, 'sender');
+            $receiver = Redis::hGet($transaction->id, 'receiver');
+            if ($sender != "customer:$customer_id") {
+                $transaction->sender->id    = Redis::hGet($sender, 'id');
+                $transaction->sender->name  = Redis::hGet($sender, 'name');
+                $transaction->sender->phone = Redis::hGet($sender, 'phone');
             }
-            $receiver = Redis::hGet($history_id, 'receiver');
-            if ($receiver != "user:$customer_id") {
-                $history->receiver->phone = Redis::hGet($receiver, 'phone');
+            if ($receiver != "customer:$customer_id") {
+                $transaction->sender->id      = Redis::hGet($sender, 'id');
+                $transaction->receiver->name  = Redis::hGet($receiver, 'name');
+                $transaction->receiver->phone = Redis::hGet($receiver, 'phone');
             }
-            $history->amount = Redis::hGet($history_id, 'amount');
-            $history->type = Redis::hGet($history_id, 'type');
-            $history->description = Redis::hGet($history_id, 'description');
-            array_push($histories, $history);
-            Redis::rPush("history:$customer_id", $history);
+            $transaction->amount = Redis::hGet($transaction->id, 'amount');
+            $transaction->description = Redis::hGet($transaction->id, 'description');
+            array_push($histories, $transaction);
+            Redis::rPush("history:$customer_id", $transaction->id);
         }
 
+        $customer->id                   = "customer:$customer_id";
         $customer->name                 = Redis::hGet("customer:$customer_id", 'name');
         $customer->phone                = Redis::hGet("customer:$customer_id", 'phone');
         $customer->email                = Redis::hGet("customer:$customer_id", 'email');
@@ -75,6 +83,25 @@ class Transaction extends Controller
 
     public function detail($transaction_id)
     {
-        return Redis::hGetAll("history:$transaction_id");
+        $transaction->id                = "transaction:$transaction_id";
+
+        $sender = Redis::hGet($transaction->id, 'sender');
+        $transaction->sender->id        = Redis::hGet($sender, 'id');
+        $transaction->sender->name      = Redis::hGet($sender, 'name');
+        $transaction->sender->phone     = Redis::hGet($sender, 'phone');
+
+        if (strpos($request->receiver, 'customer:')) {
+            $receiver = Redis::hGet($transaction->id, 'receiver');
+            $transaction->receiver->id      = Redis::hGet($receiver, 'id');
+            $transaction->receiver->name    = Redis::hGet($receiver, 'name');
+            $transaction->receiver->phone   = Redis::hGet($receiver, 'phone');
+        } else {
+            $transaction->receiver      = Redis::hGet($transaction->id, 'receiver');
+        }
+
+        $transaction->amount            = Redis::hGet($transaction->id, 'amount');
+        $transaction->description       = Redis::hGet($transaction->id, 'description');
+
+        return response()->json([ 'transaction' => $transaction ], 200);
     }
 }
